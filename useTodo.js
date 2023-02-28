0021ea134513104361c2b09e445aec6d7474cfd6
Jason <jasonpalacios1346@gmail.com>
2023-02-28 10:24:08 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from 'react';
+import {todoReducer} from '../08-useReducer/todoReducer';
+
+const init = () => {
+  return JSON.parse( localStorage.getItem('todos') ) || [];
+}
+
+export const useTodo = (initialState = []) => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState , init);
+
+    const todosCount = todos.length;
+
+    const pendingTodosCount = todos.filter( todo => !todo.done ).length;
+    
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ) );
+
+    }, [todos])
+
+    const handleNewTodo = ( todo ) => {
+
+        dispatch({
+            type: '[TODO] Add Todo',
+            payload : todo,
+        });
+    
+    }
+
+    const handleDeleteTodo = ( id ) => {
+
+        dispatch({
+        type : '[TODO] Remove Todo',
+        payload: id,
+        });
+
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+        type : '[TODO] Toggle Todo',
+        payload: id,
+        });
+    }
+
+  
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount,
+    }
+}
